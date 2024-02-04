81b17c4d2b2616ba1eec3a13f401551cd6217871
Sachin Rawat <you@example.com>
2024-02-04 19:47:43 +0530

    Place Order

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 connectDB(); // Connect to DB
 
 const app = express();
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
